5e764471709c806ab81da14e810dd0d2e6d702bd
weslley41 <41weslley@gmail.com>
2022-01-02 12:47:35 -0300

    adicionado página inicial como menu de navegação para admin

---
+++ php/view/admin/menu_filtros.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Ecommerce - adm</title>
+	<!-- Fonte -->
+	<link href="https://fonts.googleapis.com/css2?family=Rowdies:wght@300&display=swap" rel="stylesheet">
+	<!-- CSS -->
+	<link rel="stylesheet" href="../../../css/cores.css">
+	<link rel="stylesheet" href="../../../css/header.css">
+	<link rel="stylesheet" href="../../../css/home_admin.css">
+	<link rel="stylesheet" href="../../../css/header_admin.css">
+	<link rel="stylesheet" href="../../../css/switch_theme.css">
+</head>
+<body>
+	<?php
+		include_once('../../../html/header_admin.html');
+		include_once('../../../html/menu_filtros.html');
+	?>
+</body>
+</html>

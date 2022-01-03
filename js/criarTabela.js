7eac875145282224a6a137f47f8349f7b4dd0146
weslley41 <41weslley@gmail.com>
2022-01-03 12:51:37 -0300

    pequena alteração: mudança de class para id

--- js/criarTabela.js
+++ js/criarTabela.js
@@ -55,11 +55,11 @@ function criar_tabela() {
 			labels.appendChild(th)
 		});
 		let editar = document.createElement('th');
-		editar.className = 'coluna-acoes';
+		editar.id = 'coluna-editar';
 		editar.innerText = 'Editar';
 		labels.appendChild(editar);
 		let excluir = document.createElement('th');
-		excluir.className = 'coluna-acoes';
+		excluir.id = 'coluna-excluir';
 		excluir.innerText = 'Excluir';
 		labels.appendChild(excluir);
 
@@ -100,12 +100,12 @@ function criar_tabela() {
 				tr.appendChild(td);
 			});
 			let editar = document.createElement('td');
-			editar.className = 'coluna-acoes';
+			editar.id = 'coluna-editar';
 			editar.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>';
 			editar.setAttribute('onclick', 'window.location.href="editar_produto.php?id=' + produto.id + '"');
 			tr.appendChild(editar);
 			let excluir = document.createElement('td');
-			excluir.className = 'coluna-acoes';
+			excluir.id = 'coluna-excluir';
 			excluir.setAttribute('onclick', 'popupExcluirProduto(' + produto.id + ',"' + capitalize(produto.nome) + '")');
 			excluir.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="9" x2="15" y2="15"></line><line x1="15" y1="9" x2="9" y2="15"></line></svg>';
 			tr.appendChild(excluir);

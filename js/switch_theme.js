be27f22c127cf2da02c5e3c3a5335f82e8defe13
weslley41 <41weslley@gmail.com>
2022-01-05 12:12:48 -0300

    adição de cookie salvar a escolha do tema

--- js/switch_theme.js
+++ js/switch_theme.js
@@ -1,5 +1,16 @@
-let html = document.querySelector("html") 
-let checkbox = document.getElementById("switch-theme")
+let html = document.querySelector("html");
+let checkbox = document.getElementById("switch-theme");
 checkbox.addEventListener('change', function() {
-	html.classList.toggle('dark_mode')
+	html.classList.toggle('dark_mode');
+
+	let novoEstado = html.className == 'dark_mode' ? 'on' : 'off';
+	document.cookie = 'dark_mode=' + novoEstado;
 })
+
+function mudaTema() {
+	let dark_mode = document.cookie;
+	let estadoAtual = dark_mode.substring(dark_mode.indexOf('=') + 1);
+	if (estadoAtual == 'on') {
+		checkbox.click();
+	}
+}

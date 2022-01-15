1ac1fbb029d0c8973118f3611f8ef91e07ecd95e
weslley41 <41weslley@gmail.com>
2022-01-14 22:22:13 -0300

    add favicon

--- php/view/admin/utilizacoes.php
+++ php/view/admin/utilizacoes.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="/ecommerce/assets/favicon/favicon.ico" type="image/x-icon">
 	<title>Utilizações de categorias e tags</title>
 	<!-- Fonte -->
 	<link href="https://fonts.googleapis.com/css2?family=Rowdies:wght@300&display=swap" rel="stylesheet">
